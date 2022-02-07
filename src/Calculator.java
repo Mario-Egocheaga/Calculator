79fbc5988960e0fad8c9d3d1cca62255d0770015
Bing47328 <43833659+Bing47328@users.noreply.github.com>
2022-02-07 16:49:33 -0500

    Asked user for 2 values and add them

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,9 +1,15 @@
+import java.util.Scanner;
 
 public class Calculator {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Enter a Value");
+		Scanner sc = new Scanner(System.in);
+		int operand1 = sc.nextInt();
+		System.out.println("Enter a Value");
+		int operand2 = sc.nextInt();
+		System.out.println(operand1 + operand2);
 		
 
 	}
