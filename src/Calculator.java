56ce315ab19eff8c2d3b695f1ee15b6515e24862
Bing47328 <43833659+Bing47328@users.noreply.github.com>
2022-02-14 10:46:42 -0500

    Asks user to multiply and divide

--- src/Calculator.java
+++ src/Calculator.java
@@ -10,7 +10,7 @@ public class Calculator {
 		System.out.println("Enter a Value");
 		int operand2 = sc.nextInt();
 		
-		System.out.println("Would you like to add or subtract?");
+		System.out.println("Would you like to add, subtract, multiply, or divide?");
 		String option = sc.next().toLowerCase();
 		
 		if(option.equals("add"))
@@ -21,6 +21,16 @@ public class Calculator {
 		{
 			System.out.println(operand1 - operand2);
 			
+		}
+		
+		else if(option.equals("multiply"))
+		{
+			System.out.println(operand1 * operand2);
+			
+		}
+		else if (option.equals("divide"))
+		{
+			System.out.println(operand1 / operand2);
 		}
 		
 
